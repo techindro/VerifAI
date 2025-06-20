7d09bdaf876b26b5f5f54f8449f9a05ee5f11ef8
Shubham Patel 🐕(Oscar.Ai) <166341408+techindro@users.noreply.github.com>
2025-06-20 12:27:26 +0530

    Create detect.py

---
+++ backend/deepfake_model/detect.py
@@ -0,0 +1,7 @@
+# Dummy function (you can later integrate CNN model here)
+def detect_deepfake(video_path):
+    return {
+        "fake": True,
+        "confidence": 0.92,
+        "message": "This is a simulated deepfake detection result."
+    }
